f73cf51fbb82211c5502efab9873bcdd3eb3b2ac
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-04-24 22:13:10 -0300

    add navigation work

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+  return ( 
+    <NavigationContainer>
+      <SafeAreaView>
+        <Text>HOME</Text>
+      </SafeA>
+    </NavigationContainer>
+   );
+}
+ 
+export default App;
+
+
